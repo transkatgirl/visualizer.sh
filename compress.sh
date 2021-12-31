74150a777d90bb14df94d06eeeb80ffeae8f0030
Kat <10361870+katattakd@users.noreply.github.com>
2021-12-30 20:23:52 -0800

    Added files

---
+++ compress.sh
@@ -0,0 +1,8 @@
+acodec=$(ffprobe -v 0 -of csv=p=0 -select_streams a:0 -show_entries stream=codec_name "$in")
+if [ "$acodec" == "vorbis" ] || [ "$acodec" == "opus" ]; then
+	audiostr="-acodec copy"
+else
+	audiostr="-acodec libopus -b:a 512k"
+fi
+
+ffmpeg -i "output.mkv" -vcodec libvpx-vp9 -pix_fmt yuv420p -g 240 -crf 28 -vf scale=-2:1080 -r 60 $audiostr -map_metadata -1 -row-mt 1 -tile-columns 2 compressed.webm
---
+++ visualizer.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+file="$1"
+res="$2"
+vmult="$3"
+sspeed="$4"
+gamma="$5"
+bgamma="$6"
+afchain="$7"
+vfchain="$8"
+
+if [ -z "$file" ]; then
+	echo "Usage: bash visualiser.sh [audio file] (resolution) (volume multiplier) (scroll speed) (sonograph gamma) (bargraph gamma) (audio filter chain) (video filter chain)"
+	exit
+fi
+
+if [ -z "$res" ]; then
+	res="4096x3072"
+fi
+hres=$(echo "$res" | awk -F "x" '{ print $1 }')
+vres=$(echo "$res" | awk -F "x" '{ print $2 }')
+
+if [ -z "$vmult" ]; then
+	vmult="28"
+fi
+
+if [ -z "$sspeed" ]; then
+	sspeed="1.134"
+fi
+
+if [ -z "$gamma" ]; then
+	gamma="1.26"
+fi
+
+if [ -z "$bgamma" ]; then
+	bgamma="2.38"
+fi
+
+if [ -z "$afchain" ]; then
+	afchain="loudnorm=dual_mono=true:offset=12"
+fi
+
+if [ -z "$vfchain" ]; then
+	vfchain="copy"
+fi
+
+sono_h=$(echo $hres $vres | awk '{ print int( ( (100 - (( $1 * 9 * 50 ) / ( 16 * $2 ))) * $2 ) / 200 ) * 2 }')
+vconfig="cscheme=0|1|1|1|0|1:fontcolor=0x101010:font='family=sans-serif\:weight=50\:minspace=true':sono_h=$sono_h"
+visualizer="$afchain,showcqt=s=$res:r=60:csp=bt709:$vconfig:count=$(echo $sspeed $sono_h | awk '{ print ($1 * $2)/90 }'):sono_v=$vmult*a_weighting(f):sono_g=$gamma:bar_g=$bgamma:tc=$(echo $sspeed | awk '{ print 0.17 / $1 }'),$vfchain"
+
+ffmpeg -i "$file" -pix_fmt yuv444p -acodec copy -vcodec libx264 -crf 0 -preset medium -filter_complex "$visualizer" output.mkv
