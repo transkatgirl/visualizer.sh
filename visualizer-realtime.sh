fc766c6022796cedc7329dde7c57b1d8a8a8255f
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-09-17 10:55:37 -0700

    Tweaked parameters

--- visualizer-realtime.sh
+++ visualizer-realtime.sh
@@ -19,7 +19,7 @@ if [ -z "$res" ]; then
 	#res="4096x3072"
 	#res="1920x1440"
 	#res="1440x1080"
-	res="1280x720"
+	res="1280x960"
 fi
 hres=$(echo "$res" | awk -F "x" '{ print $1 }')
 vres=$(echo "$res" | awk -F "x" '{ print $2 }')
