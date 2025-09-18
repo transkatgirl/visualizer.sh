f53bc542a3778de9e0945e9cef6ebe644860ebea
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-09-18 03:08:33 -0700

    Added timeclamp parameter

--- visualizer-realtime.sh
+++ visualizer-realtime.sh
@@ -3,12 +3,13 @@ file="$1"
 res="$2"
 gamma="$3"
 sspeed="$4"
-bgamma="$5"
-afchain="$6"
-vfchain="$7"
+tclamp="$5"
+bgamma="$6"
+afchain="$7"
+vfchain="$8"
 
 if [ -z "$file" ]; then
-	echo "Usage: bash visualiser-realtime.sh [audio device] (resolution) (gamma) (scroll speed) (bargraph gamma) (audio filter chain) (video filter chain)"
+	echo "Usage: bash visualiser-realtime.sh [audio device] (resolution) (gamma) (scroll speed) (timeclamp) (bargraph gamma) (audio filter chain) (video filter chain)"
 	echo ""
 	ffmpeg -f avfoundation -list_devices true -hide_banner -i ""
 	exit
@@ -25,8 +26,11 @@ hres=$(echo "$res" | awk -F "x" '{ print $1 }')
 vres=$(echo "$res" | awk -F "x" '{ print $2 }')
 
 if [ -z "$sspeed" ]; then
-	#sspeed="1.134"
 	sspeed="2"
+fi
+
+if [ -z "$tclamp" ]; then
+	tclamp="1"
 fi
 
 if [ -z "$gamma" ]; then
@@ -49,6 +53,6 @@ fi
 
 sono_h=$(echo $hres $vres | awk '{ print int( ( (100 - (( $1 * 9 * 50 ) / ( 16 * $2 ))) * $2 ) / 200 ) * 2 }')
 vconfig="cscheme=0|1|1|1|0|1:fontcolor=0x101010:font='family=sans-serif\:weight=50\:minspace=true':sono_h=$sono_h"
-visualizer="$afchain,showcqt=s=$res:r=60:csp=bt709:$vconfig:count=$(echo $sspeed $sono_h | awk '{ print ($1 * $2)/90 }'):bar_v=16*a_weighting(f):sono_v=16*a_weighting(f):sono_g=$gamma:bar_g=$bgamma:tc=$(echo $sspeed | awk '{ print 0.17 / $1 }'),$vfchain"
+visualizer="$afchain,showcqt=s=$res:r=60:csp=bt709:$vconfig:count=$(echo $sspeed $sono_h | awk '{ print ($1 * $2)/90 }'):bar_v=16*a_weighting(f):sono_v=16*a_weighting(f):sono_g=$gamma:bar_g=$bgamma:tc=$(echo $sspeed $tclamp | awk '{ print (0.17 / $1) * $2 }'),$vfchain"
 
 ffmpeg -f avfoundation -i "none:$file" -f wav - | ffplay -an -fflags nobuffer -f lavfi "amovie=f=pipe:filename=\'pipe:0\',$visualizer[out0]"
--- visualizer.sh
+++ visualizer.sh
@@ -3,12 +3,13 @@ file="$1"
 res="$2"
 gamma="$3"
 sspeed="$4"
-bgamma="$5"
-afchain="$6"
-vfchain="$7"
+tclamp="$5"
+bgamma="$6"
+afchain="$7"
+vfchain="$8"
 
 if [ -z "$file" ]; then
-	echo "Usage: bash visualiser.sh [audio file] (resolution) (gamma) (scroll speed) (bargraph gamma) (audio filter chain) (video filter chain)"
+	echo "Usage: bash visualiser.sh [audio file] (resolution) (gamma) (scroll speed) (timeclamp) (bargraph gamma) (audio filter chain) (video filter chain)"
 	exit
 fi
 
@@ -23,8 +24,11 @@ hres=$(echo "$res" | awk -F "x" '{ print $1 }')
 vres=$(echo "$res" | awk -F "x" '{ print $2 }')
 
 if [ -z "$sspeed" ]; then
-	#sspeed="1.134"
 	sspeed="2"
+fi
+
+if [ -z "$tclamp" ]; then
+	tclamp="1"
 fi
 
 if [ -z "$gamma" ]; then
@@ -46,6 +50,6 @@ fi
 
 sono_h=$(echo $hres $vres | awk '{ print int( ( (100 - (( $1 * 9 * 50 ) / ( 16 * $2 ))) * $2 ) / 200 ) * 2 }')
 vconfig="cscheme=0|1|1|1|0|1:fontcolor=0x101010:font='family=sans-serif\:weight=50\:minspace=true':sono_h=$sono_h"
-visualizer="$afchain,showcqt=s=$res:r=60:csp=bt709:$vconfig:count=$(echo $sspeed $sono_h | awk '{ print ($1 * $2)/90 }'):bar_v=16*a_weighting(f):sono_v=16*a_weighting(f):sono_g=$gamma:bar_g=$bgamma:tc=$(echo $sspeed | awk '{ print 0.17 / $1 }'),$vfchain"
+visualizer="$afchain,showcqt=s=$res:r=60:csp=bt709:$vconfig:count=$(echo $sspeed $sono_h | awk '{ print ($1 * $2)/90 }'):bar_v=16*a_weighting(f):sono_v=16*a_weighting(f):sono_g=$gamma:bar_g=$bgamma:tc=$(echo $sspeed $tclamp | awk '{ print (0.17 / $1) * $2 }'),$vfchain"
 
 ffmpeg -i "$file" -pix_fmt yuv444p -acodec copy -vcodec libx264 -crf 0 -preset medium -filter_complex "[0:a]$visualizer" output.mkv
