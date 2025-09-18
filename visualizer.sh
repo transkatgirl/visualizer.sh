94d0634567c6554d593b903eb5b3904a41835aa9
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-09-18 02:49:03 -0700

    Tweaked parameters

--- visualizer.sh
+++ visualizer.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 file="$1"
 res="$2"
-vmult="$3"
+gamma="$3"
 sspeed="$4"
-gamma="$5"
-bgamma="$6"
-afchain="$7"
-vfchain="$8"
+bgamma="$5"
+afchain="$6"
+vfchain="$7"
 
 if [ -z "$file" ]; then
-	echo "Usage: bash visualiser.sh [audio file] (resolution) (volume multiplier) (scroll speed) (sonograph gamma) (bargraph gamma) (audio filter chain) (video filter chain)"
+	echo "Usage: bash visualiser.sh [audio file] (resolution) (gamma) (scroll speed) (bargraph gamma) (audio filter chain) (video filter chain)"
 	exit
 fi
 
@@ -23,20 +22,13 @@ fi
 hres=$(echo "$res" | awk -F "x" '{ print $1 }')
 vres=$(echo "$res" | awk -F "x" '{ print $2 }')
 
-if [ -z "$vmult" ]; then
-	#vmult="28"
-	vmult="12.53"
-fi
-
 if [ -z "$sspeed" ]; then
 	#sspeed="1.134"
 	sspeed="2"
 fi
 
 if [ -z "$gamma" ]; then
-	#gamma="3"
-	#gamma="2.35"
-	gamma="2.718"
+	gamma="2.71828182"
 fi
 
 if [ -z "$bgamma" ]; then
@@ -44,7 +36,7 @@ if [ -z "$bgamma" ]; then
 fi
 
 if [ -z "$afchain" ]; then
-	afchain="loudnorm=dual_mono=true:offset=12"
+	afchain="loudnorm=dual_mono=true:offset=9"
 	#afchain="acopy"
 fi
 
@@ -54,6 +46,6 @@ fi
 
 sono_h=$(echo $hres $vres | awk '{ print int( ( (100 - (( $1 * 9 * 50 ) / ( 16 * $2 ))) * $2 ) / 200 ) * 2 }')
 vconfig="cscheme=0|1|1|1|0|1:fontcolor=0x101010:font='family=sans-serif\:weight=50\:minspace=true':sono_h=$sono_h"
-visualizer="[0:a]$afchain,showcqt=s=$res:r=60:csp=bt709:$vconfig:count=$(echo $sspeed $sono_h | awk '{ print ($1 * $2)/90 }'):bar_v=$vmult*a_weighting(f):sono_v=$vmult*a_weighting(f):sono_g=$gamma:bar_g=$bgamma:tc=$(echo $sspeed | awk '{ print 0.17 / $1 }'),$vfchain"
+visualizer="$afchain,showcqt=s=$res:r=60:csp=bt709:$vconfig:count=$(echo $sspeed $sono_h | awk '{ print ($1 * $2)/90 }'):bar_v=16*a_weighting(f):sono_v=16*a_weighting(f):sono_g=$gamma:bar_g=$bgamma:tc=$(echo $sspeed | awk '{ print 0.17 / $1 }'),$vfchain"
 
-ffmpeg -i "$file" -pix_fmt yuv444p -acodec copy -vcodec libx264 -crf 0 -preset medium -filter_complex "$visualizer" output.mkv
+ffmpeg -i "$file" -pix_fmt yuv444p -acodec copy -vcodec libx264 -crf 0 -preset medium -filter_complex "[0:a]$visualizer" output.mkv
